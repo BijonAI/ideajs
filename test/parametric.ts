19cc01d7d02446aac436303505ae44da9b6eb7e1
HappyFox001 <2022302121222@whu.edu.cn>
2025-02-07 15:53:03 +0800

    add parametric.ts

---
+++ test/parametric.ts
@@ -0,0 +1,28 @@
+import * as idea from "../src";
+
+const canvas = idea.field(1000, 800);
+document.body.appendChild(canvas.node());
+
+const coord = idea.plane().grid(50).axes("#ff0000").ticks(50);
+const parametric = idea.parametric(
+    (t: number) => [
+        2*Math.cos(t),
+        2*Math.sin(t)
+    ],
+    [-Math.PI, Math.PI]
+).setUnit(50).draggable();
+
+canvas.viewBox(0,0,1000,800)
+canvas.zoom(2)
+// const scaleMatrix = [
+//     [2, 0, 0],
+//     [0, 0.5, 0],
+//     [0, 0, 1]
+// ] as [[number, number, number], [number, number, number], [number, number, number]];
+
+// const line = parametric.derivative(Math.PI/2, 10).draggable();
+// coord.add(line);
+// const ellipse = parametric.matrix(scaleMatrix).showRiemannRectangles(50).draggable();
+// coord.add(ellipse);
+coord.add(parametric)
+canvas.add(coord);
